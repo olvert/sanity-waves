46c5167cb8106c71625ba4df61898462d95e0936
Olle <me@olle.io>
2021-01-18 23:20:31 +0100

    use link component in header

--- web/components/MenuItem.tsx
+++ web/components/MenuItem.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 import { MenuItem as Props } from '../generated/graphql';
 
 const MenuItem = (props: Props): JSX.Element => {
   const { title, url } = props;
 
   return (
-    <a
-      href={url}
-      className="text-sm sm:text-base text-grayish hover:text-blackish mr-2 sm:mr-3">
+    <Link href={url}>
+      <a className="text-sm sm:text-base text-grayish hover:text-blackish mr-2 sm:mr-3">
         {title}
-    </a>
+      </a>
+    </Link>
   );
 };
 

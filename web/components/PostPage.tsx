237a4472e267939916b647e6a1ef7ab422569cb4
Olle <me@olle.io>
2021-01-22 20:55:49 +0100

    add post page component

---
+++ web/components/PostPage.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import Post from './Post';
+import SiteLayout from './SiteLayout';
+import InfiniteScroll from './InfiniteScroll';
+import { SiteSettings, Post as PostModel } from '../../studio/models';
+
+type Props = {
+  settings: SiteSettings;
+  initialPosts: PostModel[];
+  getPosts: (offset: number) => Promise<PostModel[]>
+}
+
+const PostPage = (props: Props): JSX.Element => {
+  const { settings, initialPosts, getPosts } = props;
+  const [posts, setPosts] = useState<PostModel[]>(initialPosts);
+
+  const loadMorePosts = async (): Promise<boolean> => {
+    const offset = posts.length;
+
+    const newPosts = await getPosts(offset);
+
+    setPosts([...posts, ...newPosts]);
+
+    return newPosts.length > 0;
+  };
+
+  return (
+    <SiteLayout settings={settings}>
+      <InfiniteScroll loadMore={loadMorePosts}>
+        { posts.map((post) => <Post key={post.slug.current} {...post} />) }
+      </InfiniteScroll>
+    </SiteLayout>
+  );
+};
+
+export default PostPage;
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,43 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { GetServerSideProps } from 'next';
 
-import Post from '../components/Post';
-import SiteLayout from '../components/SiteLayout';
-import InfiniteScroll from '../components/InfiniteScroll';
 import { getPosts, getSiteSettings } from '../lib/sanityQueries';
 import { SiteSettings, Post as PostModel } from '../../studio/models';
+import PostPage from '../components/PostPage';
 
 type Props = {
   settings: SiteSettings;
   initialPosts: PostModel[];
 }
 
-const Home = (props: Props): JSX.Element => {
-  const { settings, initialPosts } = props;
-  const [posts, setPosts] = useState<PostModel[]>(initialPosts);
-
-  const loadMorePosts = async (): Promise<boolean> => {
-    const offset = posts.length;
-
-    const newPosts = await getPosts(offset);
-
-    setPosts([...posts, ...newPosts]);
-
-    return newPosts.length > 0;
-  };
-
-  return (
-    <SiteLayout settings={settings}>
-      <InfiniteScroll loadMore={loadMorePosts}>
-        { posts.map((post) => <Post key={post.slug.current} {...post} />)
-        }
-      </InfiniteScroll>
-      <button onClick={loadMorePosts}>
-        Load more
-      </button>
-    </SiteLayout>
-  );
-};
+const Home = (props: Props): JSX.Element => <PostPage getPosts={getPosts} {...props} />;
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const settingsPromise = getSiteSettings();

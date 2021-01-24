70a5e437bb5356c2fee59650a4798d4ea4bbe10e
Olle <me@olle.io>
2021-01-24 20:32:08 +0100

    use sanity cdn

--- web/lib/sanityClient.ts
+++ web/lib/sanityClient.ts
@@ -3,5 +3,5 @@ import sanityClient from '@sanity/client';
 export default sanityClient({
   projectId: '3n6cqkfs', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
-  useCdn: false, // `false` if you want to ensure fresh data
+  useCdn: true, // `false` if you want to ensure fresh data
 });

0068791842288f5f82a4e39fda31afdc2b991acc
Olle <me@olle.io>
2021-02-18 23:05:07 +0100

    fix footer to bottom

--- web/components/SiteLayout.tsx
+++ web/components/SiteLayout.tsx
@@ -14,23 +14,25 @@ const SiteLayout = (props: Props): JSX.Element => {
   const { siteTitle } = settings;
 
   return (
-    <div>
+    <React.Fragment>
       <Head>
         <title>{siteTitle}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto lg:mx-0 px-2 lg:px-4 xl:pl-8 pt-4 sm:pt-6 xl:pt-10 xl:pr-32">
-        <Header {...settings} />
-        { children }
-      </main>
+      <div className="container min-h-screen flex flex-col justify-between mx-auto lg:mx-0 px-2 lg:px-4 xl:pl-8 xl:pr-32">
+        <main className="pt-4 sm:pt-6 xl:pt-10">
+          <Header {...settings} />
+          { children }
+        </main>
 
-      <footer className="container mx-auto lg:mx-0 px-2 lg:px-4 xl:pl-8 xl:pr-32 mb-2">
-        <p className="text-grayish text-center text-xxs sm:text-xs py-3">
-          {getVersion()}
-        </p>
-      </footer>
-    </div>
+        <footer className="my-2">
+          <p className="text-grayish text-center text-xxs sm:text-xs">
+            {getVersion()}
+          </p>
+        </footer>
+      </div>
+    </React.Fragment>
   );
 };
 

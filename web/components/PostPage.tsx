03848719f54c2052c44c84f9f08b0214be5b9d84
Olle <me@olle.io>
2021-02-21 21:51:43 +0100

    add dynamic meta title

--- web/components/PostPage.tsx
+++ web/components/PostPage.tsx
@@ -3,16 +3,17 @@ import React, { useState } from 'react';
 import Post from './Post';
 import SiteLayout from './SiteLayout';
 import InfiniteScroll from './InfiniteScroll';
-import { SiteSettings, Post as PostModel } from '../lib/models';
+import { SiteSettings, Post as PostModel, Tag } from '../lib/models';
 
 type Props = {
   settings: SiteSettings;
   initialPosts: PostModel[];
-  getPosts: (offset: number) => Promise<PostModel[]>
+  tag?: Tag;
+  getPosts: (offset: number) => Promise<PostModel[]>;
 }
 
 const PostPage = (props: Props): JSX.Element => {
-  const { settings, initialPosts, getPosts } = props;
+  const { initialPosts, getPosts, ...rest } = props;
   const [posts, setPosts] = useState<PostModel[]>(initialPosts);
 
   const loadMorePosts = async (): Promise<boolean> => {
@@ -26,7 +27,7 @@ const PostPage = (props: Props): JSX.Element => {
   };
 
   return (
-    <SiteLayout settings={settings}>
+    <SiteLayout {...rest}>
       <InfiniteScroll loadMore={loadMorePosts} threshold={300}>
         { posts.map((post) => <Post key={post.slug.current} {...post} />) }
       </InfiniteScroll>
--- web/components/SiteLayout.tsx
+++ web/components/SiteLayout.tsx
@@ -1,41 +1,61 @@
-import { useRouter } from 'next/router';
+import { NextRouter, useRouter } from 'next/router';
 import Head from 'next/head';
 import React from 'react';
-import { SiteSettings } from '../lib/models';
+import { SiteSettings, Tag } from '../lib/models';
 import { getHost, getVersion } from '../lib/utils';
 import Header from './Header';
 
 type Props = {
-  children: React.ReactNode,
-  settings: SiteSettings,
+  children: React.ReactNode;
+  settings: SiteSettings;
+  tag?: Tag;
 }
 
-const renderMeta = (settings: SiteSettings, path: string): JSX.Element => {
+const getMetaTitleWithPageName = (siteTitle: string, pageTitle?: string): string => `${pageTitle} — ${siteTitle}`;
+
+const parseMetaTitle = (siteTitle: string, pathName: string, tag?: Tag): string => {
+  switch (pathName) {
+    case '/tags':
+      return getMetaTitleWithPageName(siteTitle, 'Tags');
+    case '/playlist':
+      return getMetaTitleWithPageName(siteTitle, 'Playlist');
+    case '/tag/[slug]':
+      return getMetaTitleWithPageName(siteTitle, tag.title);
+    case '/':
+      return siteTitle;
+    default:
+      return siteTitle;
+  }
+};
+
+const renderMeta = (settings: SiteSettings, router: NextRouter, tag?: Tag): JSX.Element => {
   const { metaDescription, siteTitle } = settings;
   const host = getHost();
 
+  const metaTitle = parseMetaTitle(siteTitle, router.pathname, tag);
+
   return (
     <React.Fragment>
-      <title>{siteTitle}</title>
+      <title>{metaTitle}</title>
       <meta name="description" content={metaDescription} />
 
-      <meta property="og:title" content={siteTitle} />
+      <meta property="og:title" content={metaTitle} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={`${host}${path}`} />
+      <meta property="og:url" content={`${host}${router.asPath}`} />
       <meta property="og:image" content={`${host}/api/ogimage`} />
     </React.Fragment>
   );
 };
 
 const SiteLayout = (props: Props): JSX.Element => {
-  const { children, settings } = props;
+  const { children, settings, tag } = props;
   const router = useRouter();
 
   return (
     <React.Fragment>
       <Head>
-        {renderMeta(settings, router.asPath)}
+        {renderMeta(settings, router, tag)}
       </Head>
 
       <div className="container min-h-screen flex flex-col justify-between mx-auto lg:mx-0 px-2 lg:px-4 xl:pl-8 xl:pr-32">

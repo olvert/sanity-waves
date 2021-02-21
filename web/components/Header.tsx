ab13d441a1c55147f44f8a0597ab01b63ea76f15
Olle <me@olle.io>
2021-02-21 20:52:37 +0100

    change site title formatting once again

--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import { SiteSettings } from '../lib/models';
 import MenuItem from './MenuItem';
 
+const formatSiteTitle = (title: string): string => `| ${title.split('').join(' ')} |`;
+
 const Header = (props: SiteSettings): JSX.Element => {
   const { siteTitle, menuItems } = props;
 
@@ -10,7 +12,7 @@ const Header = (props: SiteSettings): JSX.Element => {
     <div>
       <Link href="/">
         <a className="text-2xl sm:text-3xl sm:leading-normal inline-block -ml-1">
-          {`| ${siteTitle} |`}
+          {formatSiteTitle(siteTitle)}
         </a>
       </Link>
       <nav role="navigation" className="mt-2 sm:mt-3 mb-4 sm:mb-6 lg:mb-8">
